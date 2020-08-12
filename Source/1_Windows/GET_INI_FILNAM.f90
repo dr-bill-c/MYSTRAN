cad330f6760b5d3310774b23cf336346af0558c9
Bill Case <dbcase29@gmail.com>
2020-08-12 18:45:20 -0400

    Fixed Error

--- Source/1_Windows/GET_INI_FILNAM.f90
+++ Source/1_Windows/GET_INI_FILNAM.f90
@@ -1,8 +1,32 @@
 ! ##################################################################################################################################
+! Begin MIT license text.                                                                                    
+! _______________________________________________________________________________________________________
+                                                                                                         
+! Copyright 2019 Dr William R Case, Jr (dbcase29@gmail.com)                                              
+                                                                                                         
+! Permission is hereby granted, free of charge, to any person obtaining a copy of this software and      
+! associated documentation files (the "Software"), to deal in the Software without restriction, including
+! without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+! copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to   
+! the following conditions:                                                                              
+                                                                                                         
+! The above copyright notice and this permission notice shall be included in all copies or substantial   
+! portions of the Software and documentation.                                                                              
+                                                                                                         
+! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS                                
+! OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                            
+! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                            
+! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                                 
+! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                          
+! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN                              
+! THE SOFTWARE.                                                                                          
+! _______________________________________________________________________________________________________
+                                                                                                        
+! End MIT license text.                                                                                      
  
       SUBROUTINE GET_INI_FILNAM  ( MYSTRAN_DIR, MYSTRAN_DIR_LEN, INIFIL_NAME_LEN )
  
-! Gets name (incl path) of the MYSTRAN.INI initialization file. This is the Linux version which uses '/' as a folder seperator 
+! Gets name (incl path) of the MYSTRAN.INI initialization file. This is the Windows version which uses '\' as a folder seperator 
 
       USE PENTIUM_II_KIND, ONLY       :  BYTE, LONG
       USE IOUNT1, ONLY                :  FILE_NAM_MAXLEN, F04, INIFIL, WRT_LOG
@@ -27,7 +51,7 @@
 
       IF (MYSTRAN_DIR_LEN > 0) THEN
          INIFIL(1:MYSTRAN_DIR_LEN)  = MYSTRAN_DIR
-         INIFIL(MYSTRAN_DIR_LEN+1:MYSTRAN_DIR_LEN+2)  = '/'
+         INIFIL(MYSTRAN_DIR_LEN+1:MYSTRAN_DIR_LEN+2)  = '\'
          INIFIL(MYSTRAN_DIR_LEN+2:) = FILNAM
          INIFIL_NAME_LEN = MYSTRAN_DIR_LEN +1 + LEN(PROG_NAME) + 4
       ELSE
--- Source/1_Windows/READ_CL.f90
+++ Source/1_Windows/READ_CL.f90
@@ -1,9 +1,33 @@
 ! ##################################################################################################################################
+! Begin MIT license text.                                                                                    
+! _______________________________________________________________________________________________________
+                                                                                                         
+! Copyright 2019 Dr William R Case, Jr (dbcase29@gmail.com)                                              
+                                                                                                         
+! Permission is hereby granted, free of charge, to any person obtaining a copy of this software and      
+! associated documentation files (the "Software"), to deal in the Software without restriction, including
+! without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+! copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to   
+! the following conditions:                                                                              
+                                                                                                         
+! The above copyright notice and this permission notice shall be included in all copies or substantial   
+! portions of the Software and documentation.                                                                              
+                                                                                                         
+! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS                                
+! OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                            
+! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                            
+! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                                 
+! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                          
+! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN                              
+! THE SOFTWARE.                                                                                          
+! _______________________________________________________________________________________________________
+                                                                                                        
+! End MIT license text.                                                                                      
  
       SUBROUTINE READ_CL ( FILNAM, NC_FILNAM )
  
-! Gets command line string, which is the name of a file (FILNAM), and counts the number (NC_FILNAM) of characters
-! (leading blanks ignored) in the name. 
+! Calls GET_COMMAND_LINE to get command line string, when MYSTRAN is invoked, (which should be the name of the input file),
+! and counts the number (NC_FILNAM) of characters in the name (leading blanks ignored). 
  
       USE PENTIUM_II_KIND, ONLY       :  LONG
       USE IOUNT1, ONLY                :  FILE_NAM_MAXLEN
@@ -15,22 +39,27 @@
       CHARACTER(LEN=*), INTENT(OUT)   :: FILNAM            ! File name on command line
  
       INTEGER(LONG), INTENT(OUT)      :: NC_FILNAM         ! Length, in chars, of FILNAM (with leading blanks removed)
-      INTEGER(LONG)                   :: STATUS            ! Status from GETARG. If /= -1, it is the length of the argument
+      INTEGER(LONG)                   :: I                 ! DO loop index
  
-      INTRINSIC                       :: GETARG
+      INTRINSIC                       :: GET_COMMAND_ARGUMENT
 
 ! **********************************************************************************************************************************
 ! Initialize outputs
 
-      NC_FILNAM = 0
-      FILNAM(1:FILE_NAM_MAXLEN) = ' '
+      NC_FILNAM = FILE_NAM_MAXLEN
 
-! Get command line string which should contain the file name of the input data deck. 
+      FILNAM(1:FILE_NAM_MAXLEN) = ' '
  
-      CALL GETARG ( 1, FILNAM, STATUS )
-      IF (STATUS /= -1) THEN
-         NC_FILNAM = STATUS
+      CALL GET_COMMAND_ARGUMENT (1, FILNAM )
+ 
+      DO I=FILE_NAM_MAXLEN,1,-1
+         IF (FILNAM(I:I) /= ' ') THEN
+            EXIT
+         ELSE
+            NC_FILNAM = NC_FILNAM - 1
          ENDIF
+      ENDDO   
+ 
       RETURN
  
       END SUBROUTINE READ_CL
