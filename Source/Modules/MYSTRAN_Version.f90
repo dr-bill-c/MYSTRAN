805b66a4ab47a981c3670821fffb1d2c31c7966c
Bill Case <dbcase29@gmail.com>
2020-08-30 22:05:55 -0400

    Version Update

--- Source/Modules/MYSTRAN_Version.f90
+++ Source/Modules/MYSTRAN_Version.f90
@@ -49,9 +49,9 @@
       SAVE
 
       CHARACTER(256*BYTE)            :: MYSTRAN_COMMENT  = '*** Please report any problems to the author at dbcase29@gmail.com ***'
-      CHARACTER(  8*BYTE), PARAMETER :: MYSTRAN_VER_NUM  = '11.0'
-      CHARACTER(  3*BYTE), PARAMETER :: MYSTRAN_VER_MONTH= 'Jun'
-      CHARACTER(  2*BYTE), PARAMETER :: MYSTRAN_VER_DAY  = '22'
+      CHARACTER(  8*BYTE), PARAMETER :: MYSTRAN_VER_NUM  = '11.1'
+      CHARACTER(  3*BYTE), PARAMETER :: MYSTRAN_VER_MONTH= 'Aug'
+      CHARACTER(  2*BYTE), PARAMETER :: MYSTRAN_VER_DAY  = '30'
       CHARACTER(  4*BYTE), PARAMETER :: MYSTRAN_VER_YEAR = '2020'
       CHARACTER( 33*BYTE), PARAMETER :: MYSTRAN_AUTHOR   = 'MYSTRAN developed by Dr Bill Case'
 
