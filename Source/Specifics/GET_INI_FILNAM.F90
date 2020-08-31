8739a59852f9e805dc670acc21eb84e49faa8dc7
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2020-08-31 17:22:41 -0300

    get rid of OS-specific files

--- Source/Specifics/GET_INI_FILNAM.F90
+++ Source/Specifics/GET_INI_FILNAM.F90
@@ -26,25 +26,47 @@
 
 ! ##################################################################################################################################
 
-      SUBROUTINE GET_MYSTRAN_DIR ( MYSTRAN_DIR, MYSTRAN_DIR_LEN )
+      SUBROUTINE GET_INI_FILNAM  ( MYSTRAN_DIR, MYSTRAN_DIR_LEN, INIFIL_NAME_LEN )
 
-      USE PENTIUM_II_KIND, ONLY       :  BYTE, LONG, DOUBLE
-      USE IOUNT1, ONLY                :  FILE_NAM_MAXLEN
+! Gets name (incl path) of the MYSTRAN.INI initialization file.
 
-      USE GET_MYSTRAN_DIR_USE_IFs
+      USE PENTIUM_II_KIND, ONLY       :  BYTE, LONG
+      USE IOUNT1, ONLY                :  FILE_NAM_MAXLEN, F04, INIFIL, WRT_LOG
+      USE SCONTR, ONLY                :  BLNK_SUB_NAM, PROG_NAME
+      USE TIMDAT, ONLY                :  TSEC
+
+      USE GET_INI_FILNAM_USE_IFs
 
       IMPLICIT NONE
 
-      CHARACTER(FILE_NAM_MAXLEN*BYTE), INTENT(OUT) :: MYSTRAN_DIR       ! Directory where program executable (and INI file) exist
+      CHARACTER(LEN=LEN(PROG_NAME)+4) :: FILNAM            ! File name for MYSTRAN initialization file (not including path)
 
-      INTEGER(LONG), INTENT(OUT)                   :: MYSTRAN_DIR_LEN   ! Length of MYSTRAN_DIR (not including trailing blanks)
-      INTEGER(LONG)                                :: I                 ! DO loop index
+                                                           ! Directory where MYSTRAN executable (and INI file) exist
+      CHARACTER(FILE_NAM_MAXLEN*BYTE), INTENT(IN):: MYSTRAN_DIR
+      CHARACTER :: OS_DIR_SEP = '/'                        ! Directory separator
 
-      INTRINSIC                                    :: GET_ENVIRONMENT_VARIABLE
+      INTEGER(LONG), INTENT(IN)       :: MYSTRAN_DIR_LEN   ! Length of MYSTRAN_DIR (not including trailing blanks)
+      INTEGER(LONG), INTENT(OUT)      :: INIFIL_NAME_LEN   ! Length of INI file name (incl path)
 
 ! **********************************************************************************************************************************
-      CALL GET_ENVIRONMENT_VARIABLE ( 'MYSTRAN_directory', MYSTRAN_DIR, MYSTRAN_DIR_LEN  )
+      FILNAM( 1:LEN(PROG_NAME)) = PROG_NAME
+      FILNAM(LEN(PROG_NAME)+1:LEN(PROG_NAME)+4) = '.INI'
+
+      ! set the directory separator to '\' iff targeting Windows
+#ifdef _WIN32
+      OS_DIR_SEP = '\'
+#endif
+
+      IF (MYSTRAN_DIR_LEN > 0) THEN
+         INIFIL(1:MYSTRAN_DIR_LEN)  = MYSTRAN_DIR
+         INIFIL(MYSTRAN_DIR_LEN+1:MYSTRAN_DIR_LEN+2)  = '/'
+         INIFIL(MYSTRAN_DIR_LEN+2:) = FILNAM
+         INIFIL_NAME_LEN = MYSTRAN_DIR_LEN +1 + LEN(PROG_NAME) + 4
+      ELSE
+         INIFIL = FILNAM
+         INIFIL_NAME_LEN = LEN(PROG_NAME) + 4
+      ENDIF
 
 ! **********************************************************************************************************************************
 
-      END SUBROUTINE GET_MYSTRAN_DIR
+      END SUBROUTINE GET_INI_FILNAM
