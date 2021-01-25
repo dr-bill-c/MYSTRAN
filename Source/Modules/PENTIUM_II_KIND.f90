e055ebf8fdb059ec6c39efdf8e76e648302cd771
dr-bill-c <13693292+dr-bill-c@users.noreply.github.com>
2021-01-25 12:02:30 -0500

    Add DBL_LONG for SuperLU array factors

--- Source/Modules/PENTIUM_II_KIND.f90
+++ Source/Modules/PENTIUM_II_KIND.f90
@@ -32,11 +32,12 @@
 
       SAVE
 
-      INTEGER, PARAMETER :: BYTE   =  1
-      INTEGER, PARAMETER :: SHORT  =  2
-      INTEGER, PARAMETER :: LONG   =  4
-      INTEGER, PARAMETER :: SINGLE =  4
-      INTEGER, PARAMETER :: DOUBLE =  8
-      INTEGER, PARAMETER :: QUAD   = 16
+      INTEGER, PARAMETER              :: BYTE     =  1
+      INTEGER, PARAMETER              :: SHORT    =  2
+      INTEGER, PARAMETER              :: LONG     =  4
+      INTEGER, PARAMETER              :: DBL_LONG =  8
+      INTEGER, PARAMETER              :: SINGLE   =  4
+      INTEGER, PARAMETER              :: DOUBLE   =  8
+      INTEGER, PARAMETER              :: QUAD     = 16
 
       END MODULE PENTIUM_II_KIND 
